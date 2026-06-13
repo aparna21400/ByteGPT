7e6938d336f045b290e4b2d9acf5d03f805e7562
Aparna <aparnamishra2003@gmail.com>
2026-06-13 18:34:45 +0530

    fix: update Vite configuration for preview settings

--- FRONTEND/vite.config.js
+++ FRONTEND/vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    host: '0.0.0.0',
+    port: Number(process.env.PORT) || 4173,
+    allowedHosts: ['[bytegpt-production-efe2.up.railway.app'],
+  },
 })
